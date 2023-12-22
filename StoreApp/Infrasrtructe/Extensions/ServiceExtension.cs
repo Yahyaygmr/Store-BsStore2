b74899b960875fc2af01f0c7aeb58ce21517593d
Yahya <YAHYA@DESKTOP-4QIIH5S>
2023-12-22 18:03:41 +0300

    committed

--- StoreApp/Infrasrtructe/Extensions/ServiceExtension.cs
+++ StoreApp/Infrasrtructe/Extensions/ServiceExtension.cs
@@ -50,5 +50,14 @@ namespace StoreApp.Infrasrtructe.Extensions
             services.AddScoped<ICategoryService, CategoryManager>();
             services.AddScoped<IOrderService, OrderManager>();
         }
+
+        public static void ConfigureRouting(this IServiceCollection services)
+        {
+            services.AddRouting(options =>
+            {
+                options.LowercaseUrls = true;
+                options.AppendTrailingSlash = false;
+            });
+        }
     }
 }
